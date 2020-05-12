073033c58287acccb61b08b2bd6099d3683b3d69
chacebot <chace.medeiros@gmail.com>
2020-05-11 18:07:42 -0700

    fixed includes problem

---
+++ firmware/stm32f103c8t6/rtos/chacebot_rc_reciever/main.d
@@ -0,0 +1,46 @@
+main.o: main.c /opt/gcc-arm/arm-none-eabi/include/string.h \
+ /opt/gcc-arm/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm/arm-none-eabi/include/newlib.h \
+ /opt/gcc-arm/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/config.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc-arm/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/lock.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/cdefs.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/string.h rtos/FreeRTOS.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_stdint.h FreeRTOSConfig.h \
+ rtos/projdefs.h rtos/portable.h rtos/deprecated_definitions.h \
+ rtos/portmacro.h rtos/mpu_wrappers.h rtos/task.h rtos/list.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/cm3/nvic.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/cm3/common.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/nvic.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/timer.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/f1/timer.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6/libopencm3/include/libopencm3/stm32/common/timer_common_all.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6//rtos/libwwg/include/mcuio.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6//rtos/libwwg/include/uartlib.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6//rtos/libwwg/include/usbcdc.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdlib.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/stdlib.h \
+ /home/chace/Github/chacebot_firmware/firmware/stm32f103c8t6//rtos/libwwg/include/getline.h
